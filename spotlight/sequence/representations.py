2fdd411e8f6d609df67ac763554cd1d4a5eaba63
snemistry <smistry@tubi.tv>
2019-09-12 16:31:41 -0700

    Added TimeDistributed

--- spotlight/sequence/representations.py
+++ spotlight/sequence/representations.py
@@ -150,6 +150,29 @@ class PoolNet2(nn.Module):
     def get_embedding_size(self):
         return self.item_embeddings.embedding_dim
 
+class TimeDistributed(nn.Module):
+    def __init__(self, module, batch_first=False):
+        super(TimeDistributed, self).__init__()
+        self.module = module
+        self.batch_first = batch_first
+
+    def forward(self, x):
+
+        if len(x.size()) <= 2:
+            return self.module(x)
+
+        # Squash samples and timesteps into a single axis
+        x_reshape = x.contiguous().view(-1, x.size(-1))  # (samples * timesteps, input_size)
+
+        y = self.module(x_reshape)
+
+        # We have to reshape Y
+        if self.batch_first:
+            y = y.contiguous().view(x.size(0), -1, y.size(-1))  # (samples, timesteps, output_size)
+        else:
+            y = y.view(-1, x.size(1), y.size(-1))  # (timesteps, samples, output_size)
+
+        return y
 
 class PoolNet(nn.Module):
     """
@@ -187,7 +210,7 @@ class PoolNet(nn.Module):
                  item_embedding_layer=None,
                  sparse=False,
                  layers=[],
-                 nonlinearity='relu'):
+                 nonlinearity='tanh'):
 
         super(PoolNet, self).__init__()
 
@@ -213,7 +236,7 @@ class PoolNet(nn.Module):
         if layers and len(layers) > 0:
             self.fc_layers = torch.nn.ModuleList()
             for idx, (in_size, out_size) in enumerate(zip(layers[:-1], layers[1:])):
-                self.fc_layers.append(torch.nn.Linear(in_size, out_size))
+                self.fc_layers.append(TimeDistributed(torch.nn.Linear(in_size, out_size)))
 
     def user_representation(self, item_sequences):
         """
